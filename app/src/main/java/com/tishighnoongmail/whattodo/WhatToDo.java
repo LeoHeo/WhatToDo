1106d40d4b63e9bd7a126e3128513ba08f9cef5b
Chark0 <rscharkowicz@gmail.com>
2016-06-13 21:45:12 -0700

    Added Intent
    added on click create intent to open new activity amd created nerw
    activity layout

--- app/src/main/java/com/tishighnoongmail/whattodo/WhatToDo.java
+++ app/src/main/java/com/tishighnoongmail/whattodo/WhatToDo.java
@@ -2,13 +2,27 @@ package com.tishighnoongmail.whattodo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+
 
 public class WhatToDo extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.tishighnoon.whattodo.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_what_to_do);
     }
+    public void addbtn_click(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+     //   EditText editText = (EditText) findViewById(R.id.edit_message);
+     //   String message = editText.getText().toString();
+      //  intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+
 
 }
