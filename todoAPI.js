2d280209c82782d3f246f5774abfe050156e2610
SydBal <dominic.balassone@gmail.com>
2016-06-19 22:26:54 -0700

    added  more websites to visit

--- todoAPI.js
+++ todoAPI.js
@@ -3,6 +3,11 @@ var express = require('express')
 var app = express()
  
 app.get('/notes', function(req, res) {
+  res.json({notes: "This is your notebook. Edit this to start saving your notes!", hello:{world:"hi",um:"ya"}})
+})
+
+
+app.get('/', function(req, res) {
   res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
 })
  
