10f41739716581ae482c58cc6e8a6c9c9214ec86
SydBal <dominic.balassone@gmail.com>
2016-06-19 22:53:29 -0700

    heroku port added

--- index.js
+++ index.js
@@ -6,18 +6,13 @@ app.get('/notes', function(req, res) {
   res.json({notes: "This is your notebook. Edit this to start saving your notes!", hello:{world:"hi",um:"ya"}})
 })
 
-
 app.get('/', function(req, res) {
   res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
 })
- 
-app.listen(3000, function(){
-  console.log('listening on *:3000\nvisit http://localhost:3000/notes for test GET');
-});
 
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8080, 'localhost');
-console.log('Server running at http://localhost:8080/');
+//for heroku
+var port = process.env.PORT || 3000;
+ 
+app.listen(port, function(){
+  console.log('listening on *:'+port+'\nvisit http://localhost:3000/notes for test GET');
+});
